3fffa31e90aca8e60fd2a05c23f16d236a5e40b3
Julien Tanguy <tanguy.j@sfeir.com>
2022-05-12 15:10:32 +0200

    Exo 6

--- src/ex06/Player.tsx
+++ src/ex06/Player.tsx
@@ -1,15 +1,22 @@
-import React, { useState, cloneElement } from "react";
+import React, { useState, cloneElement, useEffect } from "react";
 import { Fab } from "@rmwc/fab";
 import { range } from "../utils";
 import { PersonCard } from "../solution/PersonCard";
 
 type CarouselProps = {
+  currentIndex: number;
+  next: () => void;
+  prev: () => void;
   children: React.ReactElement[];
 };
 
-const Carousel: React.FC<CarouselProps> = ({ children }) => {
+const Carousel: React.FC<CarouselProps> = ({
+  currentIndex,
+  next,
+  prev,
+  children,
+}) => {
   const childArray = React.Children.toArray(children) as React.ReactElement[];
-  const [currentIndex, setCurrentIndex] = useState(0);
   const { pred, succ } = range(0, childArray.length - 1);
 
   const cards: [number, string][] = [
@@ -20,13 +27,13 @@ const Carousel: React.FC<CarouselProps> = ({ children }) => {
 
   return (
     <div className="flex-row">
-      <Fab icon="skip_previous" mini onClick={() => setCurrentIndex(pred)} />
+      <Fab icon="skip_previous" mini onClick={prev} />
       <div className="carousel">
         {cards.map(([i, className]) =>
           cloneElement(childArray[i], { className })
         )}
       </div>
-      <Fab icon="skip_next" mini onClick={() => setCurrentIndex(succ)} />
+      <Fab icon="skip_next" mini onClick={next} />
     </div>
   );
 };
@@ -36,17 +43,34 @@ type PlayerProps = {
 };
 
 export const Player: React.FC<PlayerProps> = ({ people }) => {
+  const { pred, succ } = range(0, people.length - 1);
+  const goNext = () => setCurrentIndex(succ);
+  const goPrev = () => setCurrentIndex(pred);
+  const [playing, setPlaying] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    if (playing) {
+      const tid = setTimeout(() => {
+        goNext();
+      }, 2000);
+      return () => clearTimeout(tid);
+    }
+  }, [playing, goNext]);
   return (
     <>
       <main>
-        <Carousel>
+        <Carousel currentIndex={currentIndex} prev={goPrev} next={goNext}>
           {people.map((person) => (
             <PersonCard person={person} key={person.id} />
           ))}
         </Carousel>
       </main>
       <footer>
-        <Fab icon="play_arrow" />
+        <Fab
+          icon={playing ? "pause" : "play_arrow"}
+          onClick={() => setPlaying(!playing)}
+        />
         {/* show the pause icon when playing */}
       </footer>
     </>
