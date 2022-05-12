5c1a8cb57b497b4f6f12e7e3bdbcdac1f20e935e
Julien Tanguy <tanguy.j@sfeir.com>
2022-05-12 16:11:03 +0200

    Exo 7

--- src/ex07/App.tsx
+++ src/ex07/App.tsx
@@ -1,31 +1,48 @@
 import React, { useState, useEffect } from "react";
 import { TopAppBarActionItem } from "@rmwc/top-app-bar";
+import { Switch, Route, Redirect, NavLink, useHistory } from "react-router-dom";
 
 import { Header } from "../solution/Header";
 import { Loading } from "../solution/Loading";
 import { SearchableList } from "../solution/SearchableList";
 import { Player } from "../solution/Player";
 import { loadPeople } from "../utils";
+import { Person } from "./Person";
 
 export const App: React.FC = () => {
-  const [showList, setShowList] = useState(true);
-  const toggleView = () => setShowList((x) => !x);
-  const toggleIcon = showList ? "view_carousel" : "view_module";
-
+  const history = useHistory();
   const [people, setPeople] = useState<People>([]);
   useEffect(() => {
     loadPeople().then(setPeople);
   }, []);
 
-  const CurrentView: React.ComponentType<{ people: People }> =
-    people.length === 0 ? Loading : showList ? SearchableList : Player;
+  const toList = () => history.push("/list");
 
   return (
     <>
       <Header>
-        <TopAppBarActionItem icon={toggleIcon} onClick={toggleView} />
+        <NavLink to="/player">
+          <TopAppBarActionItem icon={"view_carousel"} />
+        </NavLink>
+        <TopAppBarActionItem icon={"view_module"} onClick={toList} />
       </Header>
-      <CurrentView people={people} />
+      {people.length === 0 ? (
+        <Loading />
+      ) : (
+        <Switch>
+          <Route path="/player" component={() => <Player people={people} />} />
+          <Route
+            path="/list"
+            render={() => <SearchableList people={people} />}
+          />
+          <Route path="/person/:id">
+            {({ match }) => (
+              <Person person={people.find((p) => p.id === match.params.id)} />
+            )}
+          </Route>
+          <Redirect to="/list" />
+        </Switch>
+      )}
     </>
   );
 };
--- src/ex07/Person.tsx
+++ src/ex07/Person.tsx
@@ -1,3 +1,23 @@
 import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import { PersonCard } from "../solution/PersonCard";
 
-export const Person: React.FC = () => null;
+type PersonProps = {
+  person: Person;
+};
+
+export const Person: React.FC<PersonProps> = ({ person }) => {
+  const history = useHistory();
+  if (person) {
+    return <PersonCard person={person} />;
+  } else {
+    const goBack = (ev) => history.goBack();
+    return (
+      <div>
+        Person not found
+        <Link to="/list">Return to list</Link>
+        <button onClick={goBack}>Go Back</button>
+      </div>
+    );
+  }
+};
