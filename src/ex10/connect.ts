e50ae9daec56948ff6f9c54d12c711108d9a293e
Julien Tanguy <tanguy.j@sfeir.com>
2022-05-13 16:34:26 +0200

    Exo10

--- src/ex10/connect.ts
+++ src/ex10/connect.ts
@@ -1,20 +1,40 @@
-import { connect } from "react-redux";
+import { connect, useDispatch, useSelector } from "react-redux";
 import { savePerson, loadPeople } from "../utils";
 import { State, Action } from "./state";
 import { Dispatch } from "redux";
 
+function selector(state: State): { people: People } {
+  return { people: Object.values(state.people) };
+}
 // provide people from state
-export const withPeople = connect((state: State) => ({}));
+export const withPeople = connect(selector);
 
 // provide loading from state
-// and the loadPeople function dispatching SET_PEOPLE
+// and the loadPeople function dispatching PEOPLE_LOADED
 export const withLoading = connect(
-  (state: State) => ({}),
-  (dispatch: Dispatch<Action>) => ({})
+  (state: State) => ({ loading: state.people == null }), // mapStateToProps
+  (dispatch: Dispatch<Action>) => ({
+    //mapDispatchToProps
+    loadPeople: () => {
+      loadPeople().then((value) =>
+        dispatch({ type: "PEOPLE_LOADED", payload: value })
+      );
+    },
+  })
 );
 
 // provide person from state using useSelector
-export const usePerson = (id: string) => null;
+export const usePerson = (id: string): Person => {
+  const person = useSelector<State, Person>((state) => state.people[id]);
+  return person;
+};
 
 // and the onUpdate callback dispatching SET_PERSON using useDispatch
-export const useUpdatePerson = () => (person: Person) => Promise.reject();
+export const useUpdatePerson = () => {
+  const dispatch = useDispatch();
+  return (person: Person) => {
+    return savePerson(person).then((newPerson) => {
+      dispatch({ type: "PERSON_UPDATED", person: newPerson });
+    });
+  };
+};
--- src/ex10/state.ts
+++ src/ex10/state.ts
@@ -1,18 +1,37 @@
 import { Reducer } from "redux";
 
-export type State = {};
+type PersonId = string;
 
-export type Action = { type: "SET_PEOPLE" } | { type: "SET_PERSON" };
+export type State = {
+  people: Record<PersonId, Person> | null;
+};
 
-const initialState = {};
+export type Action =
+  | { type: "PEOPLE_LOADED"; payload: People }
+  | { type: "PERSON_UPDATED"; person: Person };
+
+const initialState = {
+  people: null,
+};
 
 export const reducer: Reducer<State, Action> = (
   state = initialState,
   action
 ) => {
   switch (action.type) {
-    case "SET_PEOPLE":
-    case "SET_PERSON":
+    case "PEOPLE_LOADED":
+      return {
+        ...state,
+        people: Object.fromEntries(action.payload.map((p) => [p.id, p])),
+      };
+    case "PERSON_UPDATED":
+      return {
+        ...state,
+        people: {
+          ...state.people,
+          [action.person.id]: action.person,
+        },
+      };
     default:
       return state;
   }
