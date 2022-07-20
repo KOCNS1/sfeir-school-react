ec2ddeb1d894f0e32167452fb95bc04f21110a70
Victor Jozwicki <victor.jozwicki@gmail.com>
2022-07-20 12:30:01 +0200

    feat(linkButton): Customized link button

--- libs/design/src/buttons/button/button.tsx
+++ libs/design/src/buttons/button/button.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import { HTMLAttributes } from 'react';
-import styles from './button.module.scss';
+import styles from '../button.module.scss';
 
 /* eslint-disable-next-line */
 export type ButtonProps = HTMLAttributes<HTMLButtonElement>;
--- libs/design/src/index.ts
+++ libs/design/src/index.ts
@@ -1,4 +1,5 @@
-export * from './lib/button/button';
+export * from './buttons/link-button/link-button';
+export * from './buttons/button/button';
 export * from './lib/panel/panel';
 export * from './lib/grid/grid';
 export * from './lib/iconized/iconized';
