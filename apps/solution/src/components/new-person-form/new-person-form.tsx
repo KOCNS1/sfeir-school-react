5d09e26f6248415f6442d2b6043c23d09458bf85
Victor Jozwicki <victor.jozwicki@gmail.com>
2022-07-20 18:33:47 +0200

    feat(useForm): Extract form logic in a custom hook

--- apps/solution/src/components/new-person-form/new-person-form.tsx
+++ apps/solution/src/components/new-person-form/new-person-form.tsx
@@ -1,46 +1,36 @@
 import { Button, Input, Title } from '@libs/design';
-import { FormEvent, useState } from 'react';
+import { FormEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { PersonModel, postPeople } from '../../api/person';
+import { postPeople } from '../../api/person';
 import styles from './new-person-form.module.scss';
+import useForm from './useForm';
 
 /* eslint-disable-next-line */
 export function NewPersonForm() {
-  const [inputValues, setInputValues] = useState<PersonModel>({
+  const navigate = useNavigate()
+  const { values, handleChange: handleChangeNew } = useForm({
     id: '',
     firstname: '',
     lastname: '',
     photo: '',
     position: ''
   })
-
-  const navigate = useNavigate()
   function onSubmit(e: FormEvent<HTMLFormElement>): void {
     e.preventDefault()
-
-    postPeople(inputValues).then(res => {
+    postPeople(values).then(res => {
       console.log(res);
       navigate('/people')
     })
-  }
-
-  /**
-   * Controls the inputs https://reactjs.org/docs/forms.html#controlled-components
-   * @param e Event object that contains the value of the input
-   */
-  function handleChange(e: React.FormEvent<HTMLInputElement>) {
-    const { name, value } = e.currentTarget
-    setInputValues(prevState => ({ ...prevState, [name]: value }))
   }
 
   return (
     <div className={"container " + styles['newPerson']}>
       <Title>Add new person</Title>
       <form onSubmit={(e) => onSubmit(e)}>
-        <Input type="text" name="firstname" label="First Name" value={inputValues.firstname} onChange={handleChange} mandatory />
-        <Input type="text" name="lastname" label="Last Name" value={inputValues.lastname} onChange={handleChange} mandatory />
-        <Input type="text" name="photo" label="Picture URL" value={inputValues.photo} onChange={handleChange} mandatory />
-        <Input type="text" name="position" label="Position" value={inputValues.position} onChange={handleChange} />
+        <Input type="text" name="firstname" label="First Name" value={values.firstname} onChange={handleChangeNew} required />
+        <Input type="text" name="lastname" label="Last Name" value={values.lastname} onChange={handleChangeNew} required />
+        <Input type="text" name="photo" label="Picture URL" value={values.photo} onChange={handleChangeNew} required />
+        <Input type="text" name="position" label="Position" value={values.position} onChange={handleChangeNew} />
         {/*
         "entryDate": "04/10/2015",
         "birthDate": "22/01/1963",
--- libs/design/src/lib/input/input.tsx
+++ libs/design/src/lib/input/input.tsx
@@ -8,13 +8,13 @@ export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   mandatory?: boolean
 }
 
-export function Input({ name, label, mandatory = false, value, onChange, ...passThrough }: InputProps) {
+export function Input({ name, label, required = false, value, onChange, ...passThrough }: InputProps) {
   return (
     <div className={styles['field-group']} data-cy={'field-group' + name}>
-      <label htmlFor={name} className={styles['label'] + ' ' + (mandatory ? styles['mandatory'] : '')}>
+      <label htmlFor={name} className={styles['label'] + ' ' + (required ? styles['mandatory'] : '')}>
         {label}
       </label>
-      <input name={name} {...passThrough} className={styles['input']} value={value} onChange={onChange} />
+      <input name={name} required={required} {...passThrough} className={styles['input']} value={value} onChange={onChange} />
     </div>
   );
 }
