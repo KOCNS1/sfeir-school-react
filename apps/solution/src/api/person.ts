5a7a04413e8728e58139e256712adaf66545cb18
Victor Jozwicki <victor.jozwicki@gmail.com>
2022-07-20 18:04:02 +0200

    feat(api): New POST api

--- apps/solution/src/api/person.ts
+++ apps/solution/src/api/person.ts
@@ -19,3 +19,12 @@ export interface PersonModel {
 export async function getPeople(): Promise<PersonModel[]> {
   return fetch(`${PEOPLE_API}`).then((response) => response.json());
 }
+
+export async function postPeople(data: PersonModel): Promise<PersonModel> {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  };
+  return fetch(`${PEOPLE_API}`, requestOptions).then((response) => response.json());
+}
