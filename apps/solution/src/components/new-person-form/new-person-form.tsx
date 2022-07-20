1cbf0d46541f6e2b6666a7837fa612eaa5398dce
Victor Jozwicki <victor.jozwicki@gmail.com>
2022-07-20 18:06:17 +0200

    feat(newpersonform): Handle submit to database

--- apps/solution/src/components/new-person-form/new-person-form.tsx
+++ apps/solution/src/components/new-person-form/new-person-form.tsx
@@ -1,24 +1,46 @@
 import { Button, Input, Title } from '@libs/design';
 import { FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { PersonModel, postPeople } from '../../api/person';
 import styles from './new-person-form.module.scss';
 
 /* eslint-disable-next-line */
-export interface NewPersonFormProps { }
+export function NewPersonForm() {
+  const [inputValues, setInputValues] = useState<PersonModel>({
+    id: '',
+    firstname: '',
+    lastname: '',
+    photo: '',
+    position: ''
+  })
 
-export function NewPersonForm(props: NewPersonFormProps) {
-  const [payload, setPayload] = useState({})
+  const navigate = useNavigate()
   function onSubmit(e: FormEvent<HTMLFormElement>): void {
     e.preventDefault()
-    console.log("Test", e)
+
+    postPeople(inputValues).then(res => {
+      console.log(res);
+      navigate('/people')
+    })
   }
+
+  /**
+   * Controls the inputs https://reactjs.org/docs/forms.html#controlled-components
+   * @param e Event object that contains the value of the input
+   */
+  function handleChange(e: React.FormEvent<HTMLInputElement>) {
+    const { name, value } = e.currentTarget
+    setInputValues(prevState => ({ ...prevState, [name]: value }))
+  }
+
   return (
     <div className={"container " + styles['newPerson']}>
       <Title>Add new person</Title>
       <form onSubmit={(e) => onSubmit(e)}>
-        <Input type="text" name="firstName" label="First Name" value="test" mandatory />
-        <Input type="text" name="lastName" label="Last Name" mandatory />
-        <Input type="text" name="pictureURL" label="Picture URL" mandatory />
-        <Input type="text" name="position" label="Position" />
+        <Input type="text" name="firstname" label="First Name" value={inputValues.firstname} onChange={handleChange} mandatory />
+        <Input type="text" name="lastname" label="Last Name" value={inputValues.lastname} onChange={handleChange} mandatory />
+        <Input type="text" name="photo" label="Picture URL" value={inputValues.photo} onChange={handleChange} mandatory />
+        <Input type="text" name="position" label="Position" value={inputValues.position} onChange={handleChange} />
         {/*
         "entryDate": "04/10/2015",
         "birthDate": "22/01/1963",
