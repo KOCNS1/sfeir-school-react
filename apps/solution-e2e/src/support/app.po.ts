5f562f0e68476906ee2452cf7c915525cfd1ef0d
Victor Jozwicki <victor.jozwicki@gmail.com>
2022-07-20 18:05:51 +0200

    test: Missing test functions

--- apps/solution-e2e/src/support/app.po.ts
+++ apps/solution-e2e/src/support/app.po.ts
@@ -1,5 +1,6 @@
 export const getGreeting = () => cy.get('h1');
 export const getSearchBar = () => cy.get('[role="search"]');
+export const getInput = (id) => cy.get(`[data-cy='input-${id}']`);
 export const findPersonPanel = (id) => cy.get(`[data-cy='person-panel-${id}']`)
 export const findLeanneWoodardPanel = () => findPersonPanel('5763cd4d9d2a4f259b53c901')
 export const findReginaldMarche = () => findPersonPanel('5763cd4dfa6f96cd26c65780')
@@ -9,3 +10,11 @@ export const findReginaldMarche = () => findPersonPanel('5763cd4dfa6f96cd26c6578
  * @param text
  */
 export const typeInSearchBar = (text: string) => getSearchBar().get('input').clear().type(text);
+
+/**
+ * Find an input and type in it (cleans everytime)
+ * @param text to type
+ * @param id to find
+ * @returns
+ */
+export const typeInInput = (text: string, id: string) => getInput(id).clear().type(text)
