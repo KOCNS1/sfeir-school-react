db8c792119464587144e958bd72e97ca0eca55be
Victor Jozwicki <victor.jozwicki@gmail.com>
2022-07-20 12:42:11 +0200

    refractor(libs): Buttons weren't in the lib

--- libs/design/src/index.ts
+++ libs/design/src/index.ts
@@ -1,5 +1,5 @@
-export * from './buttons/link-button/link-button';
-export * from './buttons/button/button';
+export * from './lib/buttons/link-button/link-button';
+export * from './lib/buttons/button/button';
 export * from './lib/panel/panel';
 export * from './lib/grid/grid';
 export * from './lib/iconized/iconized';
