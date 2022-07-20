123820a85db373575f748856e8f958ccd10e2cd3
Victor Jozwicki <victor.jozwicki@gmail.com>
2022-07-20 13:41:59 +0200

    fix(test): Link needs to be in a router

--- libs/design/src/lib/buttons/link-button/link-button.spec.tsx
+++ libs/design/src/lib/buttons/link-button/link-button.spec.tsx
@@ -1,10 +1,11 @@
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 
 import LinkButton from './link-button';
 
 describe('LinkButton', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<LinkButton to="/">Go to</LinkButton>);
+    const { baseElement } = render(<BrowserRouter><LinkButton to="/">Go to</LinkButton></BrowserRouter>);
     expect(baseElement).toBeTruthy();
   });
 });
